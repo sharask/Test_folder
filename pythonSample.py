552c71f5b1bd23f6aaf38dd8cd979056d9df8ab8
sharask <sarunask@gmail.com>
2024-01-11 12:21:58 +0200

    Added Features

--- pythonSample.py
+++ pythonSample.py
@@ -1,3 +1,5 @@
 print("------------------------")
-print("--- Python code test ---")
+print("--- Python code test !!! ---")
 print("------------------------")
+new_msg = "New code added"
+print(msg)
